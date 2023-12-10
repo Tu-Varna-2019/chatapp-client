e717184f688ff2107f32080badee1b9f2e296525
Iliyan Kostov <iliyan.kostov@email.ikostov.org>
2023-12-10 17:02:52 +0200

    added sonarqube dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.4" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+  id("com.android.application") version "8.1.4" apply false
+  id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+  id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+  properties {
+    property(
+      "sonar.projectKey",
+      "Tu-Varna-2019_Tu-Varna-Masters-Winter-AndroidKotlin-Project-Chat_AYwcmzPxkyMQ8V9j1VQJ"
+    )
+  }
 }
