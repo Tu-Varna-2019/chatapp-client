1c9bdde261949ba58b9f50304a42f72bbdc2b9df
Iliyan Kostov <iliyan.kostov@email.ikostov.org>
2023-12-24 11:33:33 +0200

    added background activity

--- app_iliyan/src/main/java/com/example/app_iliyan/model/LocalData.kt
+++ app_iliyan/src/main/java/com/example/app_iliyan/model/LocalData.kt
@@ -1,21 +1,32 @@
 package com.example.app_iliyan.model
 
 import android.app.Application
+import android.os.Handler
+import android.os.Looper
 
 class LocalData : Application() {
   var authenticatedUser: User? = null
+  private val handler = Handler(Looper.getMainLooper())
+  private val runnableCode: Runnable =
+    object : Runnable {
+      override fun run() {
+        // NotificationService.showNotification(applicationContext, "hi", "hi", 0)
+        handler.postDelayed(this, 5000)
+      }
+    }
 
   override fun onCreate() {
     super.onCreate()
     instance = this
+    handler.post(runnableCode)
   }
 
   companion object {
     private var instance: LocalData? = null
 
     fun setAuthenticatedUser(
-        username: String,
-        email: String,
+      username: String,
+      email: String,
     ) {
       instance?.authenticatedUser = User(username, email, "")
     }
