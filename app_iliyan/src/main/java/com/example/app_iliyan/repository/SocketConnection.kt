213e0994d1a017f4cbad38e78157980c1f8f9e6d
Iliyan Kostov <iliyan.kostov@email.ikostov.org>
2024-01-11 14:22:28 +0200

    removed not needed comments

--- app_iliyan/src/main/java/com/example/app_iliyan/repository/SocketConnection.kt
+++ app_iliyan/src/main/java/com/example/app_iliyan/repository/SocketConnection.kt
@@ -3,11 +3,7 @@ package com.example.app_iliyan.repository
 import com.example.app_iliyan.BuildConfig
 import com.example.app_iliyan.dataclass.ServerResponse
 import com.example.app_iliyan.helpers.Utils
-import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.isActive
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.BufferedReader
 import java.io.BufferedWriter
@@ -17,62 +13,31 @@ import java.net.Socket
 
 class SocketConnection {
 
-  @OptIn(DelicateCoroutinesApi::class)
   companion object {
     private const val SERVER_ADDRESS = BuildConfig.SERVER_ADDRESS
     private const val PORT = BuildConfig.PORT
 
-    private lateinit var socket: Socket
-    private lateinit var reader: BufferedReader
-    private lateinit var writer: BufferedWriter
-
-    init {
-      GlobalScope.launch(Dispatchers.IO) {
-        setupConnection()
-        listenForServerResponses()
-      }
-    }
-
-    private suspend fun setupConnection() =
+    suspend fun sendAndReceiveData(jsonString: String): ServerResponse =
       withContext(Dispatchers.IO) {
-        socket = Socket(SERVER_ADDRESS, PORT)
-        reader = BufferedReader(InputStreamReader(socket.getInputStream()))
-        writer = BufferedWriter(OutputStreamWriter(socket.getOutputStream()))
-      }
-
-    @OptIn(DelicateCoroutinesApi::class)
-    private fun listenForServerResponses() {
-      GlobalScope.launch(Dispatchers.IO) {
         try {
-          while (isActive) {
-            val serverResponse = reader.readLine()
-            if (serverResponse != null) {
-              Utils.logger.error("Received from server: $serverResponse")
-              ServerDataHandler.parseResponse(serverResponse)
+          Socket(SERVER_ADDRESS, PORT).use { socket ->
+            BufferedWriter(OutputStreamWriter(socket.getOutputStream())).use { writer ->
+              BufferedReader(InputStreamReader(socket.getInputStream())).use { reader ->
+                Utils.logger.info("Sending data: $jsonString")
+
+                writer.write(jsonString)
+                writer.newLine()
+                writer.flush()
+
+                val serverResponse = reader.readLine()
+                Utils.logger.error("Received from server: $serverResponse")
+
+                ServerDataHandler.parseResponse(serverResponse)
+              }
             }
           }
         } catch (e: Exception) {
-          Utils.logger.error("Error: SocketConnection: {}", e.message)
-        }
-      }
-    }
-
-    suspend fun sendAndReceiveData(jsonString: String): ServerResponse =
-      withContext(Dispatchers.IO) {
-        setupConnection()
-        try {
-          Utils.logger.info("Sending data: $jsonString")
-
-          writer.write(jsonString)
-          writer.newLine()
-          writer.flush()
-
-          val serverResponse = reader.readLine()
-          Utils.logger.error("Received from server: $serverResponse")
-
-          ServerDataHandler.parseResponse(serverResponse)
-        } catch (e: Exception) {
-          Utils.logger.error("Error: SocketConnection: {}", e.message)
+          Utils.logger.error("Error: SocketConnection: {}", e.printStackTrace())
           ServerDataHandler.parseResponse(
             "{\"response\":{\"status\":\"Error\",\"message\":\"Socket connection error!\"}}"
           )
